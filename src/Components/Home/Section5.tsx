631b96f2145aa20472b9376c6fe4a8f7b709cf3a
Muhammad <muhammad.sufyan@piecyfer.com>
2023-09-05 18:51:33 +0500

    section 4

---
+++ src/Components/Home/Section5.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {  Typography,Box,Stack,IconButton,Button} from "@mui/material";
+export default function Section5() {
+  return (
+    <Stack direction={'row'} marginTop={'5%'} px={5} >
+        <Box width={'700px'}>
+            <Typography fontFamily={'Oswald'} fontWeight={'bold'}  variant='h3'  gutterBottom>
+                THE HISTORY
+            </Typography>
+            <Typography fontFamily={'Oswald'} fontWeight={'bold'}  variant='body1'  gutterBottom>
+                OF DAMASCUS
+            </Typography>
+            <Typography fontFamily={'Oswald'}   variant='body1'  gutterBottom>
+            DAMASCUS IS A WELL-KNOWN STYLE OF STEEL THAT IS DISTINGUISHABLE BY ITS CONTRASTING MARBLE LIGHT AND DARK DESIGN.  THERE ARE TWO TYPES OF DAMASCUS STEEL (CAST DAMASCUS STEEL AND PATTERN-WELDED DAMASCUS STEEL).  ALTHOUGH SIMILAR LOOKING, THEY ARE FORGED THROUGH VERY SPECIFIC AND DIFFERENT PROCESSES. HISTORICALLY, DAMASCUS STEEL WAS ALWAYS PRIZED FOR ITS BEAUTY, AS WELL AS ABILITY TO KEEP A SHARP EDGE WHILE REMAINING STRONG AND FLEXIBLE. SEEN AS THE “STEEL OF ANCIENTS”, ARMS MADE OF DAMASCUS STEEL ARE SUPERIOR TO THOSE MADE OF IRON. DAMASCUS CRAFTSMANSHIP GAINED A REPUTATION OF NEAR MAGICAL PROPERTIES AND WITH ONLY A PRECIOUS FEW THAT KNEW HOW TO ACHIEVE THIS, IT WOULD BECOME A PRECIOUS PIECE OF HISTORY FOR ANY ARMS ENTHUSIAST.
+            </Typography>
+        </Box>
+        <Box>
+            
+        </Box>
+
+    </Stack>
+  )
+}
