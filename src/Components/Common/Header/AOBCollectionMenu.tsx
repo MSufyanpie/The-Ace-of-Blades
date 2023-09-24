7ae961767cce5c8bb0068805b8190774022962da
Muhammad Sufyan <sufi.sufyan121@gmail.com>
2023-09-24 15:18:40 +0500

    changing design of collection menu

--- src/Components/Common/Header/AOBCollectionMenu.tsx
+++ src/Components/Common/Header/AOBCollectionMenu.tsx
@@ -1,8 +1,74 @@
-import { Box, Menu } from '@mui/material'
+import { Box, Menu, MenuItem, Stack, Typography } from '@mui/material'
+import folding from '../../../assets/Categories/Folding Knife.jpeg'
+import ring from "../../../assets/Categories/Key Ring.jpeg";
+import kitchenKnife from "../../../assets/Categories/Kitchen Knife.jpeg";
+import KnifeCare from "../../../assets/Categories/Knife Care.png";
 
-import CategoriesPage from '../../AOB Collection/CategoriesPage'
-
+import  { useEffect, useState } from "react"
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../../Config/Firebase';
+import { Link } from 'react-router-dom';
+import { Image } from 'react-bootstrap';
 export const AOBCollectionMenu = ({anchorel,onClose}:any) => {
+  const[products,setProducts]:any=useState([{}])
+    let ProductsRef=collection(db,"AOB Collection")
+    
+    useEffect(()=>{
+        const getProductsList= async()=>{
+          try {
+            let data=await getDocs(ProductsRef)
+            const actualData=data.docs.map((doc)=>({
+            ...doc.data(),
+            id:doc.id,
+      
+            }))
+            console.log(actualData)
+            setProducts(actualData)
+            
+          } catch (error) {
+            console.error(error)
+          }
+            
+          }
+        getProductsList()
+       console.log(products)
+        
+      },[])
+      const filtered=products.filter((data:any)=>{ return data.category===1})
+      const filtered2=products.filter((data:any)=>{ return data.category===2})
+      const filtered3=products.filter((data:any)=>{ return data.category===3})
+      const filtered4=products.filter((data:any)=>{ return data.category===4})
+      var len1=filtered.length
+      var len2=filtered2.length
+      var len3=filtered3.length
+      var len4=filtered4.length
+  const categories = [
+    {
+      img: folding,
+      title: "FOLDING KNIVES",
+      number: len1+" PRODUCTS",
+      link: "/FoldingKnives",
+    },
+    {
+      img: ring,
+      title: "KEY RINGS",
+      number: len2+" PRODUCTS",
+      link: "/KeyRings",
+    },
+    {
+      img: kitchenKnife,
+      title: "KITCHEN AND CHEF KNIVES",
+      number: len4+" PRODUCTS",
+      link: "/KitchenandChefKnives",
+    },
+    {
+      img: KnifeCare,
+      title: "KNIFE CARE",
+      number: len3+" PRODUCTS",
+      link: "/KnifeCareProducts",
+    },
+  ];
+  
   return (
     
     <Menu
@@ -12,8 +78,26 @@ export const AOBCollectionMenu = ({anchorel,onClose}:any) => {
     onClose={onClose}
     MenuListProps={{ onMouseLeave: onClose }}
     >
-       <Box width={900}>
-    <CategoriesPage/></Box>
+       <Box width={700}>
+       <Stack direction={'column'}>
+    {categories.map((data:any,index:number)=>{
+      
+      return(
+        <MenuItem key={index}>
+        <Link to={data.link} style={{textDecoration:'none'}}>
+        <Stack direction={'row'} spacing={2}>
+          <Image style={{height:'50px',width:'50px'}} src={data.img}></Image>
+          <Typography sx={{color:'black',fontWeight:'bold'}}>{data.title}</Typography>
+          <Typography sx={{color:'red'}}>({data.number})</Typography>
+        </Stack>
+        </Link>
+        
+        </MenuItem>
+      )
+      
+    })}
+    </Stack>
+    </Box>
     </Menu>
     
   )
--- src/Config/Firebase.tsx
+++ src/Config/Firebase.tsx
@@ -1,28 +1,30 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 
 import {getAuth,} from "firebase/auth";
 import{getStorage}  from "firebase/storage"
 import {getFirestore} from 'firebase/firestore'
 
-
 const firebaseConfig = {
   //@ts-ignore
-  apiKey: process.env.REACT-APP-FIREBASE-API-KEY,
+  apiKey: 'AIzaSyCZKAb-izhnTzxmtWowYAJDJXMQ-hBTnrU',
   //@ts-ignore
-  authDomain: process.env.REACT-APP-FIREBASE-AUTH-DOMAIN,
+  authDomain: 'aceofblades-12971.firebaseapp.com',
   //@ts-ignore
-  projectId: process.env.REACT-APP-FIREBASE-PROJECT-ID,
+  projectId: 'aceofblades-12971',
   //@ts-ignore
-  storageBucket: process.env.REACT-APP-FIREBASE-STORAGE-BUCKET,
+  storageBucket:'aceofblades-12971.appspot.com',
   //@ts-ignore
-  messagingSenderId: process.env.REACT-APP-FIREBASE-SENDER-ID,
+  messagingSenderId:'183965776583',
   //@ts-ignore
-  appId: process.env.REACT-APP-FIREBASE-APP-ID,
+  appId:'1:183965776583:web:1a996369bc90d15b8b9ce2',
   
 };
 //@ts-ignore
+
 export const app = initializeApp(firebaseConfig);
 export const storage=getStorage(app)
 export const auth=getAuth();
 export const db=getFirestore(app)
+
