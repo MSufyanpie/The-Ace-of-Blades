c185526711f7c6f7ac86d200ef4a0600ec84970c
Muhammad <muhammad.sufyan@piecyfer.com>
2023-10-03 18:25:32 +0500

    shipping&returns page layout fixed

---
+++ src/Pages/Shipping&Returns/ShippingandReturns.tsx
@@ -0,0 +1,18 @@
+
+
+import Section1 from '../../Components/Common/Footer/FooterLinks/Shipping and Returns/Section1'
+import Design from '../../Components/Common/Footer/FooterLinks/Shipping and Returns/Design'
+import Layout from '../../Layout/Layout'
+
+
+export default function ShippingandReturns() {
+  return (
+    <div>
+        <Layout>
+        <Section1/>
+        <Design/>
+        </Layout>
+        
+    </div>
+  )
+}
